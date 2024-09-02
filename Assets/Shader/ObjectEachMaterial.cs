cd2ed537bba1d97f5f2dda1e79dacfc6141eb506
TokisakiAshita <2969125767@qq.com>
2024-09-02 19:06:11 +0800

    通用Shader:Unlit编写
    下一步负责LitShader的编写：即通用PBRShader

--- Assets/Shader/ObjectEachMaterial.cs
+++ Assets/Shader/ObjectEachMaterial.cs
@@ -7,9 +7,13 @@ public class ObjectEachMaterial : MonoBehaviour
 {
 
 	static int baseColorId = Shader.PropertyToID("_BaseColor");
+	static int cutoffId = Shader.PropertyToID("_Cutoff");
 	static MaterialPropertyBlock block;
 	[SerializeField]
 	Color baseColor = Color.white;
+	[SerializeField, Range(0f, 1f)]
+	float cutoff = 0.5f;
+
 	void OnValidate()
 	{
 		if (block == null)
@@ -17,6 +21,7 @@ public class ObjectEachMaterial : MonoBehaviour
 			block = new MaterialPropertyBlock();
 		}
 		block.SetColor(baseColorId, baseColor);
+		block.SetFloat(cutoffId, cutoff);
 		GetComponent<Renderer>().SetPropertyBlock(block);
 	}
 	void Awake()
