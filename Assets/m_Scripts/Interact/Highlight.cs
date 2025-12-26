5237894cc181bfbc5d5e02aeee7dc23641cb8f8f
E-Dawnize <157566656+E-Dawnize@users.noreply.github.com>
2025-12-26 23:27:35 +0800

    bug修改
    shader改完了
    碰撞箱调大了一点，在边缘还是会有点闪，但是不在视线中心不明显
    初始图片改了
    maincamera禁用了，lerp过程还是会撕裂
    视频打包不进去，还是不知道原因

--- Assets/m_Scripts/Interact/Highlight.cs
+++ Assets/m_Scripts/Interact/Highlight.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 public class Highlight:MonoBehaviour
 {
     private Renderer HighlightRenderer;
+    private MaterialPropertyBlock _propBlock;
+    private Color hightlightColor=new(1f, 1f, 0.8f);
+    private Color normalColor=new(1f, 1f, 1f);
     private void Start()
     {
+        _propBlock = new MaterialPropertyBlock();
         HighlightRenderer=transform.GetChild(0).GetComponent<Renderer>();
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.transform.CompareTag("MainCamera"))
+        if (other.transform.CompareTag("MainCamera"))
+        {
             EnableHighlightAndOutline();
+        }
+            
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if(other.transform.CompareTag("MainCamera"))
+        if (other.transform.CompareTag("MainCamera"))
+        {
             DisableHighlightAndOutline();
+        }
+            
     }
 
     void EnableHighlightAndOutline()
     {
-        HighlightRenderer.enabled = true;
+        HighlightRenderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetColor("_HighlightColor", hightlightColor);
+        _propBlock.SetInteger("_boolean", 1);
+        HighlightRenderer.SetPropertyBlock(_propBlock);
     }
 
     void DisableHighlightAndOutline()
     {
-        HighlightRenderer.enabled = false;
+        HighlightRenderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetColor("_HighlightColor", normalColor);
+        _propBlock.SetInteger("_boolean", 0);
+        HighlightRenderer.SetPropertyBlock(_propBlock);
     }
 }
